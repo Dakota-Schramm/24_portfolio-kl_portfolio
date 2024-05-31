975d8c87a119bd508205cc11b6f332ed028014a6
Dakota Schramm <william.schramm11@gmail.com>
2024-05-31 10:54:15 -0500

    Add links to education section to navigation

--- components/Menu/Navigation.tsx
+++ components/Menu/Navigation.tsx
@@ -150,6 +150,27 @@ const Navigation = () => {
               padding={2}
               marginX={2}
               as="a"
+              href="#education"
+              rel="noreferrer"
+              onClick={onMenuItemClick}
+            >
+              Education
+            </Button>
+          </Box>
+          <Box
+            width={{ base: '100%', lg: 'auto' }}
+            textAlign={{ base: 'center', lg: 'left' }}
+            marginY={{ base: 2, lg: 0 }}
+          >
+            <Button
+              fontWeight="light"
+              variant="ghost"
+              fontSize={menuButtonSize}
+              letterSpacing={2}
+              className={btnClassName}
+              padding={2}
+              marginX={2}
+              as="a"
               href="#jobs"
               rel="noreferrer"
               onClick={onMenuItemClick}
