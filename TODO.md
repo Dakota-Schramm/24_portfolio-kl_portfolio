c4cbe52b1340c95bc739ea349c1b2bfabf0dd832
Dakota Schramm <william.schramm11@gmail.com>
2024-05-31 10:24:30 -0500

    Fixup logo page

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 Fix images used for projects
 Fix favicon used
 Fix education section
-Fix header spacing with new text logo
--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,15 +1,12 @@
-import { memo, useState } from 'react'
-import { useColorMode, Image, useBreakpointValue, Text } from '@chakra-ui/react'
+import { memo } from 'react'
+import { useColorMode, Text } from '@chakra-ui/react'
 import Link from 'next/link'
-import { motion, AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import styles from './styles.module.css'
-import { ThemeMode, mobileBreakpointsMap } from 'config/theme'
-import { simpleOpacity } from 'config/animations'
+import { ThemeMode } from 'config/theme'
 
 const Logo = () => {
   const { colorMode } = useColorMode()
-  const MotionImage = motion(Image)
-  const isMobile = useBreakpointValue(mobileBreakpointsMap)
 
   return (
     <AnimatePresence>
